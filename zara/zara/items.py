35b3adacc9585540ba33f9de6266bb9b9d19c472
Сергей Ковальский <237264@mail.ru>
2023-07-24 11:22:22 +0300

    old price

--- zara/zara/items.py
+++ zara/zara/items.py
@@ -9,4 +9,14 @@ import scrapy
 class ZaraItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    # article = scrapy.Field()
+    name = scrapy.Field()
+    price = scrapy.Field()
+    # price_old = scrapy.Field()
+    # is_active = scrapy.Field()
+    # description = scrapy.Field()
+    # categories = scrapy.Field()
+    # images = scrapy.Field()
+    # properties = scrapy.Field()
+    # url = scrapy.Field()
+    # added = scrapy.Field()
--- zara/zara/spiders/zara_wemen_tshirt.py
+++ zara/zara/spiders/zara_wemen_tshirt.py
@@ -1,10 +1,24 @@
 import scrapy
+from ..items import ZaraItem
 
 
 class ZaraWemenTshirtSpider(scrapy.Spider):
     name = "zara_wemen_tshirt"
-    allowed_domains = ["www.zara.com"]
-    start_urls = ["https://www.zara.com/kz/ru/zhenshchiny-futbolki-l1362.html?v1=2290973"]
+    start_urls = [
+        "https://www.zara.com/kz/ru/zhenshchiny-futbolki-l1362.html?v1=2290973",
+        "https://www.zara.com/kz/ru/zhenshchiny-futbolki-l1362.html?v1=2287894",
+    ]
 
     def parse(self, response):
-        pass
+        items = ZaraItem()
+
+        name = response.css("h3 ::text").extract()[1:-4]
+        price = [
+            (price.replace("\xa0", "")).replace(" KZT", "")
+            for price in response.css(".money-amount__main ::text").extract()
+        ]
+        items["name"] = name
+        items["price"] = price
+        # items['added'] = date
+
+        yield items
