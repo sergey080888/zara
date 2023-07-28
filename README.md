86c89a1561cba1be18a165d81be881538775d159
Сергей Ковальский <237264@mail.ru>
2023-07-28 16:49:31 +0300

    final

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ```bash
 python -m pip install -r requirements.txt
 ```
+
+```bash
+cd zara
+scrapy crawl zara_final -O 1.xlsx
+```
--- zara/zara/settings.py
+++ zara/zara/settings.py
@@ -108,3 +108,6 @@ DOWNLOAD_DELAY = 4
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+FEED_EXPORTERS = {
+    "xlsx": "scrapy_xlsx.XlsxItemExporter",
+}
